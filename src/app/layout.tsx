00daa09a3bfc69586381e9b31a9f60173162c991
kishore <kishore.mongar@aptagrim.com>
2025-03-11 11:16:21 +0530

    feat: added favicon and manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,27 @@ const cormorant = cormorantImport({
 export const metadata: Metadata = {
   title: 'Kishore Mongar',
   description: 'Frontend developer',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [{ url: '/favicon/apple-touch-icon.png' }],
+    other: [
+      {
+        url: '/favicon/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      { url: '/site.webmanifest', rel: 'manifest' },
+    ],
+  },
 };
 
 export default function RootLayout({
